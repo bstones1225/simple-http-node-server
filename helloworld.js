1c79e6d54f77c80e04210e48e6c06274230a2b07
Brian Stones <bstones1225@hotmail.com>
2018-12-18 17:41:32 -0600

    initial commit

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
+  if (err) throw err;
+
+  console.log('success')
+});
+}).listen(8080);
